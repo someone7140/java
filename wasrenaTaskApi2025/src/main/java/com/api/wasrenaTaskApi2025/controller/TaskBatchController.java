2b88c80921141e6874500ca570ed37fd20ae40c6
someone7140 <ti7140@gmail.com>
2025-07-12 02:12:31 +0900

    修正

--- wasrenaTaskApi2025/src/main/java/com/api/wasrenaTaskApi2025/controller/TaskBatchController.java
+++ wasrenaTaskApi2025/src/main/java/com/api/wasrenaTaskApi2025/controller/TaskBatchController.java
@@ -5,6 +5,7 @@ import com.api.wasrenaTaskApi2025.service.task.TaskCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,11 @@ public class TaskBatchController {
     @MutationMapping
     public boolean executeTaskCheckNotifyBatch(@Argument String batchToken) {
         return taskCheckService.taskCheckNotify(batchToken);
+    }
+
+    // バッチで生存確認する用のクエリ
+    @QueryMapping
+    public boolean pingCheckExecute() {
+        return true;
     }
 }
